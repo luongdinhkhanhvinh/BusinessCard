e82da4b69bc70bcdf000d358c195d654b254ccfc
VinhKhanh <vinhldk@devblock.net>
2021-07-08 14:34:53 +0700

    Business Card Digital using QR code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # BusinessCard
+
+- You are the change value connect DB at install/index.php
---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+$config['db']['host'] = 'localhost';
+$config['db']['name'] = 'demo';
+$config['db']['user'] = 'root';
+$config['db']['pass'] = 'root';
+$config['db']['pre'] = 'vc_';
+$config['db']['port'] = '';
+
+$config['version'] = '1.3';
+$config['installed'] = '1';
+?>
